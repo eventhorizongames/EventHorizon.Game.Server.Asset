25e695529939761810f94773c6e13db6390092e9
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-08-06 21:03:28 -0500

    Task: Implement Platform Logging Implemented Serilog Logging,
    AB#138 AB#139

--- src/EventHorizon.Game.Server.Asset/Program.cs
+++ src/EventHorizon.Game.Server.Asset/Program.cs
@@ -1,7 +1,13 @@
 namespace EventHorizon.Game.Server.Asset
 {
+    using System;
+
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Hosting;
+
+    using Serilog;
+    using Serilog.Sinks.Elasticsearch;
 
     public class Program
     {
@@ -15,6 +21,56 @@ namespace EventHorizon.Game.Server.Asset
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
+                    webBuilder.UseSerilog((ctx, cfg) => cfg
+                        .Enrich.WithProperty("EnvironmentName", ctx.HostingEnvironment.EnvironmentName)
+                        .Enrich.WithProperty("ProcessIdentifier", "main")
+                        .Enrich.WithProperty("PlatformId", ctx.Configuration["OwnerDetails:PlatformId"])
+                        .Enrich.WithProperty("Host", ctx.Configuration["HOST"])
+                        .Enrich.WithProperty("ServiceName", "Asset")
+                        .Enrich.WithProperty("ApplicationVersion", ctx.Configuration["APPLICATION_VERSION"])
+                        .ReadFrom.Configuration(ctx.Configuration)
+                        .ConfigureElasticsearchLogging(ctx)
+                    );
                 });
+    }
+
+    public static class SerilogElasticsearchExtensions
+    {
+        public static LoggerConfiguration ConfigureElasticsearchLogging(
+            this LoggerConfiguration loggerConfig,
+            WebHostBuilderContext context
+        )
+        {
+            if (context.Configuration.GetValue<bool>("Serilog:Elasticsearch:Enabled"))
+            {
+                var sinkOptions = new ElasticsearchSinkOptions(
+                    new Uri(
+                        context.Configuration["Elasticsearch:Uri"]
+                    )
+                )
+                {
+                    ModifyConnectionSettings = conn =>
+                    {
+                        conn.BasicAuthentication(
+                            context.Configuration["Elasticsearch:Username"],
+                            context.Configuration["Elasticsearch:Password"]
+                        );
+                        return conn;
+                    }
+
+                };
+                context.Configuration.GetSection(
+                    "Serilog:Elasticsearch"
+                ).Bind(
+                    sinkOptions
+                );
+                return loggerConfig.WriteTo.Elasticsearch(
+                    sinkOptions
+                );
+            }
+
+            return loggerConfig;
+        }
     }
 }
